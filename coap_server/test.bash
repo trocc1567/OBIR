18c7561adc081bb44b153684d74d807b90d3aa55
trocc1567 <54438666+trocc1567@users.noreply.github.com>
2021-01-26 00:27:59 +0100

    Update test.bash

--- coap_server/test.bash
+++ coap_server/test.bash
@@ -1,8 +1,4 @@
 coap-client -m get coap://192.168.0.143:5683/.well-known/core
-coap-client -m get coap://192.168.0.143:5683/time
-coap-client -m get coap://192.168.0.143:5683/colour
-echo -n "20 276 -30" | coap-client -m put coap://192.168.0.143:5683/colour -f-
-coap-client -m get coap://192.168.0.143:5683/colour
 echo -n "n n *" | coap-client -m put coap://192.168.0.143:5683/rpn -f-
 coap-client -m get coap://192.168.0.143:5683/rpn?wyr=1&n=3
 for i in {1...10}
